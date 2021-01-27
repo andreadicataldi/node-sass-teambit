eb0cef7977d9f9fcceecff24869c22da6bbe3595
Andrea Di Cataldi <andrea.dicataldi@gmail.com>
2021-01-27 10:42:58 +0100

    jumbotron complete

--- webpack.mix.js
+++ webpack.mix.js
@@ -2,4 +2,4 @@
 
 let mix = require("laravel-mix");
 
-mix.js("src/app.js", "src").sass("src/app.scss", "src").setPublicPath("dist");
+mix.js("src/app.js", "js").sass("src/app.scss", "css").setPublicPath("dist");
